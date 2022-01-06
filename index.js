f2d9f185b7daecfe437fdc6bdb9d2658ae406455
Mikko Hyyryläinen <mikko.e.hyyrylainen@gmail.com>
2022-01-06 18:32:09 +0200

    add exercise 3.3

--- index.js
+++ index.js
@@ -40,6 +40,17 @@ app.get('/api/persons', (request, response) => {
   response.json(persons)
 })
 
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find(person => person.id === id)  
+  
+  if (person) {
+    return response.json(person)
+  } else {
+    response.status(404).end()
+  }  
+})
+
 app.get('/', (request, response) => {
   response.send('Phonebook app backend for Full Stack Open Course')
 })
