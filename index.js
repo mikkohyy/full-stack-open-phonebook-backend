c6507ef8fabf36fdf3e2817becc48daba9dc8ac0
Mikko Hyyryläinen <mikko.e.hyyrylainen@gmail.com>
2022-01-07 13:16:26 +0200

    add exercise 3.9

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const { request } = require('express')
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 
 morgan.token('sentdata', (request, response) => {
   requestBody = JSON.stringify(request.body)
