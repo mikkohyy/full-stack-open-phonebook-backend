b0412a9c7847a1aef2fd89b9a71e0b79d615a9ad
Mikko Hyyryläinen <mikko.e.hyyrylainen@gmail.com>
2022-01-07 20:09:28 +0200

    add exercise 3.13

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-const { request } = require('express')
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 
 const app = express()
+
+const Person = require('./models/person')
 
 app.use(express.static('build'))
 app.use(express.json())
@@ -22,28 +24,7 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms :s
   skip: (request) => request.method !== 'POST'
 }))
 
-let persons = [
-  { 
-    "name": "Arto Hellas", 
-    "id": 1,
-    "number": "040-123456"
-  },
-  { 
-    "id": 2,
-    "name": "Ada Lovelace", 
-    "number": "39-44-5323523"
-  },
-  { 
-    "id": 3,
-    "name": "Dan Abramov", 
-    "number": "12-43-234345"
-  },
-  { 
-    "id": 4,
-    "name": "Mary Poppendieck", 
-    "number": "39-23-6423122"
-  }
-]
+let persons = []
 
 const createId = () => {
   const newId = Math.floor(Math.random() * 10000)
@@ -96,7 +77,9 @@ app.get('/info', (request, response) => {
 })
 
 app.get('/api/persons', (request, response) => {
-  response.json(persons)
+  Person.find({}).then(persons => {
+    response.json(persons)
+  })
 })
 
 app.get('/api/persons/:id', (request, response) => {
@@ -120,7 +103,7 @@ app.delete('/api/persons/:id', (request, response) => {
   response.status(204).end()
 })
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)  
 })
